56bc758e661aca623498bd141f67b49eb14000ca
nadarm <ccc7497@gmail.com>
2019-08-02 14:00:12 +0900

    add navigation

--- app/src/main/java/com/nadarm/imagesearch/presenter/view/MainActivity.kt
+++ app/src/main/java/com/nadarm/imagesearch/presenter/view/MainActivity.kt
@@ -2,7 +2,8 @@ package com.nadarm.imagesearch.presenter.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.nadarm.imagesearch.AndroidApplication
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.nadarm.imagesearch.R
 
 
@@ -12,8 +13,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        (application as AndroidApplication).getAppComponent()
+//        (application as AndroidApplication).getAppComponent()
 
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp()
     }
 }
