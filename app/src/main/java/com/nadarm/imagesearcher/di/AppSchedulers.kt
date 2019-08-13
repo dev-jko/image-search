2dbc4b51a56680a0fa7f4e900275bed8984be418
nadarm <ccc7497@gmail.com>
2019-08-13 11:11:50 +0900

    check schedulers

--- app/src/main/java/com/nadarm/imagesearcher/di/AppSchedulers.kt
+++ app/src/main/java/com/nadarm/imagesearcher/di/AppSchedulers.kt
@@ -7,7 +7,6 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 
-// TODO  AppSchedulers 클래스를 Singleton으로 쓰면 다 같은 스케쥴러를 쓰게 되나?
 @Singleton
 class AppSchedulers @Inject constructor() {
     fun ui(): Scheduler = AndroidSchedulers.mainThread()
